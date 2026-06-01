4ae853fb77475f47bee2afaa6d035a1b9a2e7c58
emmadoub <emma.dbld@gmail.com>
2026-06-01 17:41:59 +0300

    implemented additional  suggestions

--- tests/dialog-boxes.spec.ts
+++ tests/dialog-boxes.spec.ts
@@ -11,11 +11,11 @@ test("Add and delete pet type", async ({ page }) => {
 
   await page.getByRole("button", { name: "Add" }).click();
   await expect(page.getByRole("heading", { name: "New Pet Type" }),).toBeVisible();
+  await expect(page.locator("label", { hasText: "Name" })).toBeVisible();
   const newPetTypeNameInput = page.locator("#name");
   await expect(newPetTypeNameInput).toBeVisible();
   await newPetTypeNameInput.fill("pig");
   await page.getByRole("button", { name: "Save" }).click();
-  await expect(page.locator("label", { hasText: "Name" })).toBeVisible();
   await expect(page.locator('input').last()).toHaveValue("pig");
   page.on("dialog", (dialog) => {
     expect(dialog.message()).toEqual("Delete the pet type?");
