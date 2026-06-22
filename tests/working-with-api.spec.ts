5fafe24f5c8987684f8dcd04fd82cc5b954760f2
emmadoub <emma.dbld@gmail.com>
2026-06-22 10:48:19 +0300

    Added Intercept API response test

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 import owners from '../test-data/owners.json'
 import owner2515 from '../test-data/owner2515.json'
+import specialties from '../test-data/specialties.json'
 
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/')
+    
 })
 
 
@@ -24,7 +26,6 @@ test('mocking API request', async ({ page }) => {
     await expect(page.locator('.ownerFullName')).toHaveCount(2)
 
     await page.route('**/owners/2515', async route => {
-        console.log(route.request().url())
 
         await route.fulfill({
             body: JSON.stringify(owner2515)
@@ -44,4 +45,31 @@ test('mocking API request', async ({ page }) => {
     await expect(page.locator('app-pet-list', { hasText: 'Yuki' }).locator('app-visit-list tr').filter({ has: page.locator('td') })).toHaveCount(10)
 })
 
+test('Intercept API response', async ({ page }) => {
+
+    await page.route('**/api/vets', async route => {
+
+        const response = await route.fetch()
+        const responseBody = await response.json()
+
+        responseBody[5].specialties = specialties
+
+        await route.fulfill({
+            body: JSON.stringify(responseBody)
+        })
+    })
+
+    const responsePromise = page.waitForResponse(
+        res => res.url().includes('/api/vets')
+    );
+
+    await page.getByRole("button", { name: "Veterinarians" }).click()
+    await page.getByRole("link", { name: "All" }).click()
+
+    await responsePromise;
+
+    for (let specialty of specialties){
+        await expect(page.getByRole("row", {name: "Sharon Jenkins"}).locator('td').nth(1)).toContainText(specialty.name)
+    }
+})
 
