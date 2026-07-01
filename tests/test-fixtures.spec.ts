3b9ff5592b38cc793f7cf19c3ed6510f0d9621ea
emmadoub <emma.dbld@gmail.com>
2026-07-01 19:50:13 +0300

    implemented pr suggestions

--- tests/test-fixtures.spec.ts
+++ tests/test-fixtures.spec.ts
@@ -1,18 +1,18 @@
 import { expect } from '@playwright/test';
-import { test } from '../test-options'
+import { test } from '../owners-fixtures'
 
 
 
-test('Test with fixtures', async ({ page, testData , ownersPage }) => {
+test('Test with fixtures', async ({ page, ownersPage }) => {
 
-  await page.getByText(testData.ownerName).click()
-  await expect(page.getByText(testData.visitDescription)).toBeVisible()
-  await expect(page.getByText(testData.petName)).toBeVisible()
+  await page.getByText(ownersPage.ownerName).click()
+  await expect(page.getByText(ownersPage.visitDescription)).toBeVisible()
+  await expect(page.getByText(ownersPage.petName)).toBeVisible()
   await page.getByText("Delete Visit").click()
   await page.waitForResponse('**/api/visits/*')
-  await expect(page.getByText(testData.visitDescription)).not.toBeVisible()
+  await expect(page.getByText(ownersPage.visitDescription)).not.toBeVisible()
   await page.getByText("Delete Pet").click()
   await page.waitForResponse('**/api/pets/*')
-  await expect(page.getByText(testData.petName)).not.toBeVisible()
+  await expect(page.getByText(ownersPage.petName)).not.toBeVisible()
 
 });
